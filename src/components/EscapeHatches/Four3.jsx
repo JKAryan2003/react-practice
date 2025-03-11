9ed92316c75aa6aa85ad51ec625a74b68c7d09fe
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-03-11 15:35:37 +0530

    useEffect different variations

---
+++ src/components/EscapeHatches/Four3.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+
+const Four3 = () => {
+
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(1);  
+
+  //runs on every render
+  // useEffect(() => {
+  //   alert("Runs on every render")
+  // })
+
+  //runs on 1st render
+  // useEffect(() => {
+  //   alert("Run on 1st render")
+  // }, [])
+  
+  //dependancy list changed
+  // useEffect(() => {
+  //   alert("Triggers when count is updated")
+  // }, [count])
+
+  //multiple dependancy
+  // useEffect(() => {
+  //   alert("Triggers because of multiple dependancy")
+  // }, [count, total])
+
+  // useEffect(() => {
+  //   alert("Count is updated")
+  
+  //   return () => {
+  //     alert("Count is unmounted")
+  //   }
+  // }, [count])
+  
+  
+  
+  function handleClick () {
+    setCount(count + 1)
+  }
+  
+  function hanldeTotal () {
+    setTotal(total + 1)
+  }
+  
+  return (
+    <div>
+      Count is {count}
+      <button onClick={handleClick}>
+        Counter
+      </button>
+
+      Total is {total}
+      <button onClick={hanldeTotal}>
+        Total
+      </button>
+    </div>
+  )
+}
+
+export default Four3
