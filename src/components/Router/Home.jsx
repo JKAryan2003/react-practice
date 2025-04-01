45bf5cb75db064d1310d9b982acdca067e4a999c
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-04-01 16:49:22 +0530

    implemented useNavigate

--- src/components/Router/Home.jsx
+++ src/components/Router/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/four1')
+  }
   return (
-    <div>Home</div>
+    <div>Home
+      <button onClick={handleClick}>
+        Four 1 Page
+      </button>
+    </div>
   )
 }
 
