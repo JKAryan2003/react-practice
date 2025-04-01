5b3c5f9af52d28646f2526f62e0cadbab76871c0
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-04-01 16:43:26 +0530

    used NavLink and isActive class of NavLink for css

--- src/components/Router/NavBar.jsx
+++ src/components/Router/NavBar.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import './NavBar.css' 
 const NavBar = () => {
   return (
     <div>
       <ul>
         <li>
           {/* <a href="/">Home</a> */}
-          <Link to='/'>Home</Link>
+          {/* <Link to='/'>Home</Link> */}
+          <NavLink to='/' className={({isActive}) => isActive ? "active-link" : ""}> Home</NavLink>
+
         </li>
         <li>
           {/* <a href="/four1">Four 1</a> */}
-          <Link to='/four1'>Four 1</Link>
+          {/* <Link to='/four1'>Four 1</Link> */}
+          <NavLink to='/four1' className={({isActive}) => isActive ? "active-link" : ""}>Four 1</NavLink>
         </li>
         <li>
           {/* <a href="/four2">Four 2</a> */}
-          <Link to='/four2'>Four 2</Link>
+          {/* <Link to='/four2'>Four 2</Link> */}
+          <NavLink to='/four2' className={({isActive}) => isActive ? "active-link" : ""}>Four 2</NavLink>
         </li>
         <li>
           {/* <a href="/four3">Four 3</a> */}
-          <Link to='/four3'>Four 3</Link>
+          {/* <Link to='/four3'>Four 3</Link> */}
+          <NavLink to='/four3' className={({isActive}) => isActive ? "active-link" : ""}>Four 3</NavLink>
         </li>
       </ul>
     </div>
